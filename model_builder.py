bf6f6f53476289bc113082d6ccf9f6d43a7fcb67
Pritam Das <daspritam4231@gmail.com>
2023-04-25 19:50:38 +0530

    MobileNetV3 added

--- model_builder.py
+++ model_builder.py
@@ -2,7 +2,10 @@
 Contains PyTorch model code to instantiate a VGG19 model.
 """
 import torch
-from torch import nn 
+from torch import nn
+import torch.nn.functional as F
+
+### VGG19
 
 class VGG19(nn.Module):
     """
@@ -144,6 +147,9 @@ class VGG19(nn.Module):
         return out
 
 
+
+### ResNet18
+
 class Block(nn.Module):
 
     """
@@ -238,8 +244,9 @@ class ResNet18(nn.Module):
         x = self.fc(x)
         return x 
 
-import torch
-import torch.nn as nn
+
+
+### EfficientNet
 
 class ConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0):
@@ -276,73 +283,129 @@ class EfficientNet(nn.Module):
         x = self.fc(x)
         return x
 
-import torch
-import torch.nn as nn
 
-class MobileBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1, expansion_ratio=6):
-        super(MobileBlock, self).__init__()
-        self.exp_channels = in_channels * expansion_ratio
-        self.conv1 = nn.Conv2d(in_channels, self.exp_channels, kernel_size=1, stride=1, padding=0, bias=False)
-        self.bn1 = nn.BatchNorm2d(self.exp_channels)
-        self.conv2 = nn.Conv2d(self.exp_channels, self.exp_channels, kernel_size=3, stride=stride, padding=1, groups=self.exp_channels, bias=False)
-        self.bn2 = nn.BatchNorm2d(self.exp_channels)
-        self.conv3 = nn.Conv2d(self.exp_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=False)
-        self.bn3 = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU()
 
+### MobileNetV3
+
+class Hswish(nn.Module):
     def forward(self, x):
-        identity = x
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.conv2(x)
-        x = self.bn2(x)
-        x = self.relu(x)
-        x = self.conv3(x)
-        x = self.bn3(x)
-        x += identity
-        x = self.relu(x)
-        return x
+        return x * F.relu6(x + 3, inplace=True) / 6
 
-class MobileNetV2(nn.Module):
-    def __init__(self, num_classes=10):
-        super(MobileNetV2, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=2, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(32)
-        self.layers = nn.Sequential(
-            MobileBlock(32, 16, stride=1, expansion_ratio=1),
-            MobileBlock(16, 24, stride=2),
-            MobileBlock(24, 24),
-            MobileBlock(24, 32, stride=2),
-            MobileBlock(32, 32),
-            MobileBlock(32, 32),
-            MobileBlock(32, 64, stride=2),
-            MobileBlock(64, 64),
-            MobileBlock(64, 64),
-            MobileBlock(64, 64),
-            MobileBlock(64, 96),
-            MobileBlock(96, 96),
-            MobileBlock(96, 96),
-            MobileBlock(96, 160, stride=2),
-            MobileBlock(160, 160),
-            MobileBlock(160, 160),
-            MobileBlock(160, 320),
-            nn.Conv2d(320, 1280, kernel_size=1, stride=1, padding=0, bias=False),
-            nn.BatchNorm2d(1280),
-            nn.ReLU(),
-            nn.AdaptiveAvgPool2d((1, 1))
+class Hsigmoid(nn.Module):
+    def forward(self, x):
+        return F.relu6(x + 3, inplace=True) / 6
+
+class SqueezeExcitation(nn.Module):
+    def __init__(self, in_channels, reduced_channels):
+        super(SqueezeExcitation, self).__init__()
+        self.se = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Conv2d(in_channels, reduced_channels, kernel_size=1, stride=1, padding=0),
+            Hswish(),
+            nn.Conv2d(reduced_channels, in_channels, kernel_size=1, stride=1, padding=0),
+            Hsigmoid()
         )
-        self.fc = nn.Linear(1280, num_classes)
 
     def forward(self, x):
+        se = self.se(x)
+        return x * se
+
+class InvertedResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride, expansion_factor):
+        super(InvertedResidualBlock, self).__init__()
+        hidden_dim = int(round(in_channels * expansion_factor))
+
+        self.use_residual = stride == 1 and in_channels == out_channels
+
+        layers = []
+        if expansion_factor != 1:
+            layers.append(nn.Conv2d(in_channels, hidden_dim, kernel_size=1, stride=1, padding=0, bias=False))
+            layers.append(nn.BatchNorm2d(hidden_dim))
+            layers.append(Hswish())
+
+        layers.append(nn.Conv2d(hidden_dim, hidden_dim, kernel_size=3, stride=stride, padding=1, groups=hidden_dim, bias=False))
+        layers.append(nn.BatchNorm2d(hidden_dim))
+        layers.append(Hswish())
+
+        layers.append(nn.Conv2d(hidden_dim, out_channels, kernel_size=1, stride=1, padding=0, bias=False))
+        layers.append(nn.BatchNorm2d(out_channels))
+
+        self.block = nn.Sequential(*layers)
+
+    def forward(self, x):
+        if self.use_residual:
+            return x + self.block(x)
+        else:
+            return self.block(x)
+
+class MobileNetV3_Stem(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(MobileNetV3_Stem, self).__init__()
+        self.stem = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            Hswish()
+        )
+
+    def forward(self, x):
+        return self.stem(x)
+
+class Bottlenecks(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, expansion_factor, repeat):
+        super(Bottlenecks, self).__init__()
+        layers = []
+        for i in range(repeat):
+            if i == 0:
+                layers.append(InvertedResidualBlock(in_channels, out_channels, stride, expansion_factor))
+            else:
+                layers.append(InvertedResidualBlock(out_channels, out_channels, 1, expansion_factor))
+        self.bottlenecks = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.bottlenecks(x)
+
+class Classifier(nn.Module):
+    def __init__(self, in_channels, num_classes):
+        super(Classifier, self).__init__()
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.conv1 = nn.Conv2d(in_channels, 1280, kernel_size=1, stride=1, padding=0, bias=True)
+        self.hswish1 = Hswish()
+        self.dropout = nn.Dropout(p=0.2, inplace=True)
+        self.conv2 = nn.Conv2d(1280, num_classes, kernel_size=1, stride=1, padding=0, bias=True)
+
+    def forward(self, x):
+        x = self.avgpool(x)
         x = self.conv1(x)
-        x = self.bn1(x)
-        x = nn.ReLU()(x)
-        x = self.layers(x)
+        x = self.hswish1(x)
+        x = self.dropout(x)
+        x = self.conv2(x)
         x = x.view(x.size(0), -1)
-        x = self.fc(x)
         return x
+
+class MobileNetV3(nn.Module):
+    def __init__(self, num_classes=1000):
+        super(MobileNetV3, self).__init__()
+        self.stem = MobileNetV3_Stem(3, 16)
+        self.bottlenecks = nn.Sequential(
+            InvertedResidualBlock(16, 16, 2, 1),
+            InvertedResidualBlock(16, 24, 2, 2),
+            InvertedResidualBlock(24, 40, 2, 2),
+            InvertedResidualBlock(40, 80, 2, 1),
+            InvertedResidualBlock(80, 160, 2, 2),
+            InvertedResidualBlock(160, 320, 2, 1),
+            InvertedResidualBlock(320, 640, 2, 1),
+            InvertedResidualBlock(640, 1280, 2, 1),
+        )
+        self.classifier = Classifier(1280, num_classes)
+
+    def forward(self, x):
+        x = self.stem(x)
+        x = self.bottlenecks(x)
+        x = self.classifier(x)
+        return x
+
+
+### SqueezeNet
 
 class FireModule(nn.Module):
     def __init__(self, inplanes, squeeze_planes,
