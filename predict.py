13aa019687c91e0affef33d32f7eb71a028eedb3
Pritam Das <daspritam4231@gmail.com>
2023-04-26 15:22:41 +0530

    predict updated

--- predict.py
+++ predict.py
@@ -3,6 +3,7 @@ from torchvision import transforms
 from PIL import Image
 import utils
 import argparse
+import os
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -14,7 +15,9 @@ if __name__ == "__main__":
     image = args.IMAGE
     model_name = args.MODEL.strip().lower()
 
-    if model_name == "vgg19":
+    if model_name == "all":
+        model_name = "all"
+    elif model_name == "vgg19":
         model_name = "VGG19"
 
     elif model_name == "resnet18":
@@ -49,9 +52,17 @@ if __name__ == "__main__":
     image = Image.open(image)
     image_tensor = transforms.ToTensor()(image)
     image_tensor = image_tensor.unsqueeze(dim=0).to(device)
-    model = utils.load_model("models", f"{model_name}.pth")
-    classes = model["class_names"]
-    model = model["model"].to(device)
-    pred = model(image_tensor)
-    print(f"\nBy the model {model_name}", "\nThe image is predicted to be : " + classes[pred.argmax(dim=1)])
+
+    if model_name == "all":
+        models =[model.split(".")[0] for model in os.listdir("models")]
+
+    else:
+        models = [model_name]
+
+    for model_name in models:
+        model = utils.load_model("models", f"{model_name}.pth")
+        classes = model["class_names"]
+        model = model["model"].to(device)
+        pred = model(image_tensor)
+        print(f"\nBy the model {model_name},", "\nThe image is predicted to be : " + classes[pred.argmax(dim=1)] + "\n")
 
